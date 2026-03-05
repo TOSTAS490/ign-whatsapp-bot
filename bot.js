065fac3b4a93d86ad715daa3c67c915461e6fa8e
TOSTAS490 <54781770+TOSTAS490@users.noreply.github.com>
2026-03-05 18:53:21 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,52 @@
+const { Client, LocalAuth } = require("whatsapp-web.js")
+const qrcode = require("qrcode-terminal")
+const Parser = require("rss-parser")
+
+const parser = new Parser()
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+})
+
+client.on("qr", qr => {
+    qrcode.generate(qr, { small: true })
+})
+
+client.on("ready", () => {
+    console.log("Bot ligado")
+    checkRSS()
+})
+
+client.initialize()
+
+let lastLink = null
+
+async function checkRSS() {
+    setInterval(async () => {
+
+        const feed = await parser.parseURL("https://pt.ign.com/news.xml")
+
+        const latest = feed.items[0]
+
+        if (latest.link !== lastLink) {
+
+            lastLink = latest.link
+
+            const chats = await client.getChats()
+            const group = chats.find(c => c.name === "Cãimbras FC")
+
+            if (group) {
+
+                group.sendMessage(
+`IGN 🎮
+
+${latest.title}
+
+${latest.link}`
+                )
+
+            }
+        }
+
+    }, 300000)
+}
